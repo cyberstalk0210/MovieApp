9ee132e6c13f30bc82117a068deb1e3299178790
cyberstalk0210 <biggamer0055@gmail.com>
2025-07-14 16:57:01 +0500

    Change uploadDir

--- src/main/java/com/example/movieapp/service/FileStorageService.java
+++ src/main/java/com/example/movieapp/service/FileStorageService.java
@@ -11,7 +11,7 @@ import java.nio.file.Paths;
 
 @Service
 public class FileStorageService {
-    private final String uploadRootDir = "/home/stalker/IdeaProjects/MovieApp/uploads";
+    private final String uploadRootDir = "var/www/myapp/backend/uploads";
 
     public String saveImage(String type, MultipartFile file) {
         try {
